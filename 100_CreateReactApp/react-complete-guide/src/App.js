2e646508b80dc753f10e185e9f2104c386127105
Cheetah <szuhanszky.tamas@gmail.com>
2020-07-13 23:52:17 +0200

    Adding comments - 100

--- 100_CreateReactApp/react-complete-guide/src/App.js
+++ 100_CreateReactApp/react-complete-guide/src/App.js
@@ -4,12 +4,16 @@ import './App.css';
 
 class App extends Component {
   render() {
-    /* return (
+    // This is not HTML, but JSX - JavaScript at runtime
+    // It has limitations, e.g.: class cannot be used, that's why we need to use className instead
+    // It is typical to use one root element to return, just like below - even though it is not a strict limitation
+    return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
       </div>
-    ); */
-    return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!!!'));
+    );
+    //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!!!'));
   }
 }
 
