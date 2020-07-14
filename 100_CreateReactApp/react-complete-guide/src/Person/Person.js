4f2f172af00013e40347727b910787c2c5847926
Cheetah <szuhanszky.tamas@gmail.com>
2020-07-14 22:14:18 +0200

    Outputting Dynamic Content -100

--- 100_CreateReactApp/react-complete-guide/src/Person/Person.js
+++ 100_CreateReactApp/react-complete-guide/src/Person/Person.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 // Using ES6 for its features and to make the useage of 'this' easier
 const person = () => {
-    return <p>I'm a Person!</p>
+    return <p>I'm a Person and I am {Math.floor(Math.random() * 30)} years old!</p>
 }
 
 export default person;
