98c59f8e167a2420ddafbbaa405b19790967019e
Cheetah <szuhanszky.tamas@gmail.com>
2020-07-23 14:01:28 +0200

    Revert "Reverting for class based component - 100"
    This reverts commit 2e631a2f6a69552b02f66e9f2f22fdc1b2018817.

--- 100_CreateReactApp/react-complete-guide/src/App.js
+++ 100_CreateReactApp/react-complete-guide/src/App.js
@@ -1,5 +1,4 @@
-//import React, { Component } from 'react';
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
 // Don't need "Person.js" -> the build takes care of that + 'Person' must be uppercase after the import
@@ -11,48 +10,39 @@ import Person from './Person/Person';
 
 // state property can be used due we are extending Component here - React Hooks makes it available elsewhere; state should be used with care
 // state's speciality is that if it is changes, it will lead React to update the DOM
-// class App extends Component {
-
-// Hooks are replacing, not merging the state, with Hooks we don't need to use class based components
-const app = props => { 
-  const [ personsState, setPersonsState] = useState({
+class App extends Component {
+  state = {
     persons: [
       { name: 'Max', age: 28},
       { name: 'Manu', age: 29},
       { name: 'Stephanie', age: 26}
-    ],
-    otherState: 'some other state'
-  });
+    ]
+  }
 
-  // We are doing another useState, so the otherState stays even after update, it is needed because of hooks
-  const [otherState, setOtherState] = useState('some other value')
-  console.log(personsState, otherState);
-
-  const switchNameHandler = () => {
+  switchNameHandler = () => {
     //console.log('Clicked!');
     // DON'T DO THIS: this.state.persons[0].name = 'Maximilan';
     // This is a merge, will leave any other state untouched
-    setPersonsState({
-      persons: [
-        { name: 'Maximilan', age: 28},
-        { name: 'Manu', age: 29},
-        { name: 'Stephanie', age: 27}
-      ]
-    });
-  };
+    this.setState({persons: [
+      { name: 'Maximilan', age: 28},
+      { name: 'Manu', age: 29},
+      { name: 'Stephanie', age: 27}
+    ]});
+  }
 
-//  render() {
+  render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button onClick={switchNameHandler}>Switch Name</button>
-        <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
-        <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My Hobbies: Racing</Person>
-        <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!!!'));
-  };
+  }
+}
 
-export default app;
+export default App;
