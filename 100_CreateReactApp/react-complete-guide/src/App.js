dc039e07e3d9f69d4297a5bdd3a286838c169280
Cheetah <szuhanszky.tamas@gmail.com>
2020-07-23 14:35:33 +0200

    Passing method references between components

--- 100_CreateReactApp/react-complete-guide/src/App.js
+++ 100_CreateReactApp/react-complete-guide/src/App.js
@@ -19,26 +19,34 @@ class App extends Component {
     ]
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     //console.log('Clicked!');
     // DON'T DO THIS: this.state.persons[0].name = 'Maximilan';
     // This is a merge, will leave any other state untouched
     this.setState({persons: [
-      { name: 'Maximilan', age: 28},
+      { name: newName, age: 28},
       { name: 'Manu', age: 29},
       { name: 'Stephanie', age: 27}
     ]});
   }
 
+  // .bind() syntax is prefered, the other can be inefficient
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <button onClick={() => this.switchNameHandler('Maximilian!!')}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}/>
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Max!')}>My Hobbies: Racing</Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age}/>
       </div>
     );
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!!!'));
