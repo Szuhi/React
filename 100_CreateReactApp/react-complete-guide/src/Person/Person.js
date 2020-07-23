7751a6fdc6bb695d951236ade58f64218c57419d
Cheetah <szuhanszky.tamas@gmail.com>
2020-07-23 18:03:52 +0200

    Styling with css and inline styles - 100

--- 100_CreateReactApp/react-complete-guide/src/Person/Person.js
+++ 100_CreateReactApp/react-complete-guide/src/Person/Person.js
@@ -1,11 +1,12 @@
 // We don't need a component here, since we are not going to use classes
 import React from 'react';
+import './Person.css';
 
 // Using ES6 for its features and to make the useage of 'this' easier
 // props.children refers to anything that is between the passing object's open and closing part
 const person = (props) => {
     return (
-        <div>
+        <div className="Person">
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}/>
