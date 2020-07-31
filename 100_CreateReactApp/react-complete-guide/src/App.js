d9d382b4872798c6d87e23d3cc4941164c6c908e
Cheetah <szuhanszky.tamas@gmail.com>
2020-07-31 15:02:58 +0200

    Setting class names dynamicall - 100

--- 100_CreateReactApp/react-complete-guide/src/App.js
+++ 100_CreateReactApp/react-complete-guide/src/App.js
@@ -90,11 +90,20 @@ class App extends Component {
       style.backgroundColor = 'red';
     }
 
+    //let classes = ['red', 'bold'].join(' ');
+    const classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push('red'); // classes = ['red']
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push('bold'); // classes = ['red', 'bold']
+    }
+
     // What we write here in the end will be js. We cannot use block statements like if-else etc.
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
+        <p className={classes.join(' ')}>This is really working!</p>
         <button
           style={style} 
           onClick={this.togglePersonsHandler}>Toggle Persons</button> 
